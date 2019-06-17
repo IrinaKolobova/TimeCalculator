a780bfb201b428249709025c5540ac2f4a4b3927
Irina Kolobova <kolobovairinav@gmail.com>
2019-06-17 00:17:50 -0700

    Replace calculation process for TimePickers.

--- app/src/main/java/com/example/timecalculator/MainActivity.java
+++ app/src/main/java/com/example/timecalculator/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.timecalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,9 +14,8 @@ public class MainActivity extends AppCompatActivity {
     private Button button_add, button_subtract, button_calculate;
     private TextView textView_result;
     private TimePicker timePicker_startingTime, timePicker_amountToAdd;
-    private String startingTime, amountToAdd, resultOfHours, resultOfMinutes, date;
-    private long diff;
-    private int startingHour, startingMin, addHour, addMin;
+    private String date, errorMessage, resultOfHours, resultOfMinutes;
+    private int startingHour, startingMin, addHour, addMin, diffOfHours, diffOfMinutes;
     private boolean add, subtract;
 
 
@@ -63,11 +63,57 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void calculate (View v){
-        date = "current day";
-        Toast.makeText(getApplicationContext(), "startingHour = " + startingHour + " startingMin = " + startingMin +
-                "\n" + "addHour = " + addHour + " addMin = " + addMin , Toast.LENGTH_SHORT).show();
+        date = "Current day";
+        errorMessage = "";
+        Log.i("Calculate incoming data: " ,"startingHour = " + startingHour + ", startingMin = " + startingMin +
+                "\n" + "addHour = " + addHour + ", addMin = " + addMin );
+
+        if(add){
+            diffOfHours = startingHour + addHour;
+            diffOfMinutes = startingMin + addMin;
+            if(diffOfMinutes >= 60){
+                diffOfMinutes = diffOfMinutes - 60;
+                diffOfHours += 1;
+            }
+            if(diffOfHours >= 24){
+                date = "Next day";
+                diffOfHours = diffOfHours - 24;
+            }
+        }else if(subtract){
+            diffOfHours = startingHour - addHour;
+            diffOfMinutes = startingMin - addMin;
+            if(diffOfMinutes < 0){
+                diffOfHours -= 1;
+                diffOfMinutes = 60 + diffOfMinutes;
+            }
+            if(diffOfHours < 0){
+                diffOfHours = 24 + diffOfHours;
+                date = "Previous day";
+            }
+        } else {
+            errorMessage = "Please choose add or subtract.";
+        }
+
+        resultOfHours = String.valueOf(diffOfHours);
+        resultOfMinutes = String.valueOf(diffOfMinutes);
+
+        if(diffOfHours < 10){
+            resultOfHours = "0" + resultOfHours;
+        }
+        if(diffOfMinutes < 10){
+            resultOfMinutes = "0" + resultOfMinutes;
+        }
+
+        Log.i("Calculate result data: ", "resultOfHours = " + resultOfHours + ", resultOfMinutes = " + resultOfMinutes);
+        
+        textView_result.setText(getString(R.string.textView_result, date, resultOfHours, resultOfMinutes));
+
+        add = false;
+        subtract = false;
+
 
     }
+
 
 
 }
