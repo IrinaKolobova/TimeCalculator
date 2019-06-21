ebe917c1afef8279f63c9cc0e7690a1eb2f4b638
Irina Kolobova <kolobovairinav@gmail.com>
2019-06-21 00:36:04 -0700

    Add design.

--- app/src/main/java/com/example/timecalculator/MainActivity.java
+++ app/src/main/java/com/example/timecalculator/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.timecalculator;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,12 +15,13 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button_add, button_subtract, button_calculate;
+    private ConstraintLayout constraint_layout;
+    private Button button_add, button_subtract, button_calculate, button_amPm, button_armyTime;
     private TextView textView_result;
     private TimePicker timePicker_startingTime, timePicker_amountToAdd;
     private String date, errorMessage, resultOfHours, resultOfMinutes;
     private int startingHour, startingMin, addHour, addMin, diffOfHours, diffOfMinutes;
-    private boolean add, subtract;
+    private boolean add, subtract, ampmFormat;
 
 
     @Override
@@ -24,13 +29,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        constraint_layout = findViewById(R.id.constraint_layout);
         button_add = findViewById(R.id.button_add);
         button_subtract = findViewById(R.id.button_subtract);
         button_calculate = findViewById(R.id.button_calculate);
+        button_amPm = findViewById(R.id.button_amPm);
+        button_armyTime = findViewById(R.id.button_armyTime);
         textView_result = findViewById(R.id.textView_result);
 
         timePicker_startingTime = findViewById(R.id.timePicker_startingTime);
         timePicker_startingTime.setIs24HourView(true);
+        startingHour = timePicker_startingTime.getHour();
+        startingMin = timePicker_startingTime.getMinute();
         timePicker_startingTime.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
             @Override
             public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
@@ -41,6 +51,10 @@ public class MainActivity extends AppCompatActivity {
 
         timePicker_amountToAdd = findViewById(R.id.timePicker_amountToAdd);
         timePicker_amountToAdd.setIs24HourView(true);
+        timePicker_amountToAdd.setHour(0);
+        timePicker_amountToAdd.setMinute(0);
+        startingHour = timePicker_startingTime.getHour();
+        startingMin = timePicker_startingTime.getMinute();
         timePicker_amountToAdd.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
             @Override
             public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
@@ -51,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
 
         add = false;
         subtract = false;
+        ampmFormat = false;
 
     }
 
@@ -62,11 +77,20 @@ public class MainActivity extends AppCompatActivity {
         subtract = true;
     }
 
-    public void armyTime(View v){}
+    public void armyTime(View v){
+        constraint_layout.setBackground(getDrawable(R.drawable.armytime_background));
+        timePicker_startingTime.setIs24HourView(true);
+        ampmFormat = false;
+    }
 
-    public void amPm(View v){}
+    public void amPm(View v){
+        constraint_layout.setBackground(getDrawable(R.drawable.ampm_background));
+        timePicker_startingTime.setIs24HourView(false);
+        ampmFormat = true;
+    }
 
     public void calculate (View v){
+
         date = "Current day";
         errorMessage = "";
         Log.i("Calculate incoming data: " ,"startingHour = " + startingHour + ", startingMin = " + startingMin +
@@ -110,12 +134,33 @@ public class MainActivity extends AppCompatActivity {
 
         Log.i("Calculate result data: ", "resultOfHours = " + resultOfHours + ", resultOfMinutes = " + resultOfMinutes);
 
-        textView_result.setText(getString(R.string.textView_result, date, resultOfHours, resultOfMinutes));
+        if(ampmFormat){
+            calculateAmPM(diffOfHours);
+        } else {
+            textView_result.setText(getString(R.string.textView_result, date, resultOfHours, resultOfMinutes, ""));
+        }
 
         add = false;
         subtract = false;
+    }
+
+    private void calculateAmPM(int diffOfHours) {
+        int amPmHours = diffOfHours - 12;
+        String amOrPM;
+        if(amPmHours >= 12){
+            resultOfHours = String.valueOf(amPmHours);
+            amOrPM = "PM";
+            textView_result.setText(getString(R.string.textView_result, date, resultOfHours, resultOfMinutes, amOrPM));
+        } else {
+            resultOfHours = String.valueOf(diffOfHours);
+            amOrPM = "AM";
+            textView_result.setText(getString(R.string.textView_result, date, resultOfHours, resultOfMinutes, amOrPM));
+        }
 
 
     }
-    
+
 }
+
+
+
